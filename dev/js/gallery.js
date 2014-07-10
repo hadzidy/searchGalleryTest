39c5b0eeee8a0567d2678ba949a0876d8bbcb5a0
Hadzidy Recilla <hadzidy@grupow.com>
2014-07-10 17:00:01 -0500

    porfin!!! append images from json

--- dev/js/gallery.js
+++ dev/js/gallery.js
@@ -1,20 +1,28 @@
 (function(ns){
 
 	var Galleryapp = {
+		imagesUrl: null,
+
+		urlAssing: function(){
+			var scope = this;
+			for(var i = 0; i < scope.imagesUrl.length; i++){
+				scope.photoCont.append(scope.getTemplate(scope.imagesUrl[i]));
+			}
+		},
 
 		getTemplate: function (value) {
 			var template = $("#gall-template").html();
 			return Mustache.to_html(template, value);
 		},
 	
-
 		getData: function() {
 			var scope = this;
 			$.getJSON('js/gallery.json', function(data) {
-				scope.images = data;
-				scope.photoCont.append(scope.getTemplate(scope.images.version.images[1]));
-				console.log(scope.images.version.images[1]);
-				// version.images[x];
+				scope.imagesUrl = data.version.images;
+				scope.urlAssing();
+			// for(var i = 0; i < scope.imagesUrl.length; i++){
+			// 	scope.photoCont.append(scope.getTemplate(scope.imagesUrl[i]));
+			// }
 			})
 		},		
 
